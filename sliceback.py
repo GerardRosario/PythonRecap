b7eaa5a5372ed37e0f6fbc1bbb6dc6cf514466a4
GerardRosario <55461102+GerardRosario@users.noreply.github.com>
2021-01-08 14:05:02 -0800

    Add files via upload

---
+++ sliceback.py
@@ -0,0 +1,18 @@
+letters ="abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[25::-1]
+backwards2 = letters[::-1]
+
+print(backwards)
+print(backwards2)
+
+#produce qpo
+print(letters[16:13:-1])
+
+#produce edcba
+print(letters[4::-1])
+
+#last eight numbers in reverse
+print(letters[:-9:-1])
+
+
