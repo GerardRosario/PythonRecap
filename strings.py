0791df76d03208806f122c833f4499162777e70b
GerardRosario <55461102+GerardRosario@users.noreply.github.com>
2021-01-10 14:25:49 -0800

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,25 @@
+print("today is a good day to learn python")
+print('python is fun')
+print("python's strings are easy ot use")
+print('we can even include "quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = "Nemo"
+
+print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(name + f" is {age} years old")
+print(type(age))
+
+print(f"Pi is approximately {22/ 7:12.50f}")
+pi = 22/7
+print(f"Pi is approximately {pi:12.50f}")
+
+
